0cdad6177a901ceee575d8391326564556effea4
yusuf <ysfyldz0077@gmail.com>
2024-03-12 14:22:35 +0300

    fix

--- pages/api/classes.js
+++ pages/api/classes.js
@@ -15,12 +15,35 @@ export default async function handler(req, res) {
     const newClass = await prisma.class.create({
       data: {
         name,
-        capacity,
         grade,
         section,
       },
     });
     res.status(201).json(newClass);
+  } else if (req.method === "DELETE") {
+    const { classId } = req.query;
+
+    try {
+      // Sınıfın var olup olmadığını kontrol edin
+      const existingClass = await prisma.class.findUnique({
+        where: { id: classId },
+      });
+
+      if (!existingClass) {
+        // Eğer sınıf mevcut değilse, 404 Bulunamadı sonucunu döndürün
+        return res.status(404).json({ error: 'Class not found' });
+      }
+
+      // Sınıf mevcutsa silin
+      await prisma.class.delete({
+        where: { id: classId },
+      });
+
+      res.status(204).end(); // 204 İçerik Yok, yanıt gövdesi olmadan başarılı bir silme işlemini gösterir
+    } catch (error) {
+      console.error('Error deleting class:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
