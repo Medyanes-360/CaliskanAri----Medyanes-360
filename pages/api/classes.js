87287c28115ad2637bef28e104cfb2511b125978
gokce <gokcetascii@gmail.com>
2024-03-13 17:39:23 +0300

    edited

--- pages/api/classes.js
+++ pages/api/classes.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     res.status(200).json(classes);
   } else if (req.method === "POST") {
     // Yeni sınıf oluşturma işlemi
-    const { name, capacity, grade, section } = req.body;
+    const { name, grade, section } = req.body;
     const newClass = await prisma.class.create({
       data: {
         name,
