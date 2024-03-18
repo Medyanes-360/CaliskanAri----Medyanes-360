7cc466a3089a5d5ff36d65fe22ab840d764c823f
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-03-19 01:21:11 +0300

    color bug fixed

--- components/auth/login/teacher/index.jsx
+++ components/auth/login/teacher/index.jsx
@@ -1,27 +1,26 @@
-"use client";
-import Link from "next/link";
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
 import { useState } from 'react';
-import { Formik, Form } from "formik";
-import styles from "./styles.module.css";
+import { Formik, Form } from 'formik';
+import styles from './styles.module.css';
 import { useRouter } from 'next/navigation';
-import PopupScreen from "@/components/other/popup";
+import PopupScreen from '@/components/other/popup';
 import LoadingScreen from '@/components/other/loading';
-import teacherValidationSchema from "./formikData";
-import { ToastContainer, toast } from "react-toastify";
+import teacherValidationSchema from './formikData';
+import { ToastContainer, toast } from 'react-toastify';
 
 // session: giriş yapmış kullanıcıyı temsil eder varsa bilgileri içinde barındırır.
 // signIn:  kullanıcıyı giriş yapmaya yönlendirmek için kullanılır.
-import { signIn } from "next-auth/react";
+import { signIn } from 'next-auth/react';
 
 const TeacherLoginComponent = ({ pageRole }) => {
-
   const [popupData, setPopupData] = useState({
     popupIsActive: false,
-    Title: "Uyarı.",
-    subTitle: "Bu bir uyarı bildirimidir.",
-    buttonUrl: "/",
-    buttonText: "Anasayfa"
+    Title: 'Uyarı.',
+    subTitle: 'Bu bir uyarı bildirimidir.',
+    buttonUrl: '/',
+    buttonText: 'Anasayfa',
   });
 
   const [isAccessing, setIsAccessing] = useState(false);
@@ -34,14 +33,13 @@ const TeacherLoginComponent = ({ pageRole }) => {
       <PopupScreen
         setPopupData={setPopupData}
         popupData={popupData}
-      >
-      </PopupScreen>
+      ></PopupScreen>
 
-      {isloading && (<LoadingScreen isloading={isloading} />)}
+      {isloading && <LoadingScreen isloading={isloading} />}
 
       <div className={styles.main}>
         <ToastContainer
-          position="top-right"
+          position='top-right'
           autoClose={4000}
           hideProgressBar={false}
           newestOnTop={false}
@@ -50,19 +48,18 @@ const TeacherLoginComponent = ({ pageRole }) => {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="dark"
+          theme='dark'
         />
 
         <Formik
           // input verileri
           initialValues={{
-            email: "",
-            password: "",
-            activationCode: ""
+            email: '',
+            password: '',
+            activationCode: '',
           }}
           // input check
           validationSchema={teacherValidationSchema}
-
           onSubmit={(values) => {
             setIsloading(true);
             // signIn içine hangi provider ile giriş yapılacağı ve giriş bilgileri gönderilir.
@@ -70,76 +67,75 @@ const TeacherLoginComponent = ({ pageRole }) => {
               email: values.email,
               password: values.password,
               role: pageRole,
-              callbackUrl: "/",
+              callbackUrl: '/',
               redirect: false,
             }).then((res) => {
               if (!res) {
-                toast.error("Bir hata oluştu. Lütfen tekrar deneyiniz.");
+                toast.error('Bir hata oluştu. Lütfen tekrar deneyiniz.');
                 setIsloading(false);
-              }
-
-              else if (!res.ok) {
+              } else if (!res.ok) {
                 toast.error(res.error);
                 setIsloading(false);
 
                 // verifyEmail şuanda nextauth error içerisinden gelmiyor kontrol et.
-                if (res.error.includes("doğrulanmamış") || res.error.includes("doğrulayınız")) {
+                if (
+                  res.error.includes('doğrulanmamış') ||
+                  res.error.includes('doğrulayınız')
+                ) {
                   setPopupData({
-
                     popupIsActive: true,
-                    Title: "Mail Adresiniz Doğrulanmamış!",
-                    subTitle: "Girdiğiniz mail adresi henüz doğrulanmamış. Mail adresinize gelen doğrulama kodunu girerek hesabınızı aktif edebilir, veya aşağıdaki butona basarak yeni bir doğrulama maili talep edebilirsiniz.",
-                    buttonUrl: "/auth/sendVerifyEmail",
-                    buttonText: "Mail Doğrulama"
+                    Title: 'Mail Adresiniz Doğrulanmamış!',
+                    subTitle:
+                      'Girdiğiniz mail adresi henüz doğrulanmamış. Mail adresinize gelen doğrulama kodunu girerek hesabınızı aktif edebilir, veya aşağıdaki butona basarak yeni bir doğrulama maili talep edebilirsiniz.',
+                    buttonUrl: '/auth/sendVerifyEmail',
+                    buttonText: 'Mail Doğrulama',
                   });
                 }
-              }
-              else {
+              } else {
                 setIsAccessing(true);
                 setIsloading(false);
-                toast.success("Giriş Başarılı (Yönlendiriliyorsunuz...)")
+                toast.success('Giriş Başarılı (Yönlendiriliyorsunuz...)');
                 const timeOut = setInterval(() => {
                   router.push('/');
                   clearInterval(timeOut);
                 }, 2000);
               }
-            })
-
-
+            });
           }}
         >
-
           {(props) => (
-            <Form onSubmit={props.handleSubmit} className={`${isAccessing ? "blur" : ""} ${styles.main_container}`} >
-
+            <Form
+              onSubmit={props.handleSubmit}
+              className={`${isAccessing ? 'blur' : ''} ${
+                styles.main_container
+              }`}
+            >
               <div className={styles.container}>
                 <div className={styles.container_left_side}>
                   <img
                     className={styles.left_side_image}
-                    src="https://source.unsplash.com/user/erondu/1600x900"
-                    alt="img"
+                    src='https://source.unsplash.com/user/erondu/1600x900'
+                    alt='img'
                   />
                 </div>
                 <div className={styles.container_right_side}>
-                  <div className="w-full">
+                  <div className='w-full'>
                     <div className={styles.right_side_logo}>
-                      <div
-                        className={styles.right_side_logoImage}
-                      >
+                      <div className={styles.right_side_logoImage}>
                         <Image
-                          src="/logo.png"
-                          width="150"
-                          height="150"
-                          alt="logo"
+                          src='/logo.png'
+                          width='150'
+                          height='150'
+                          alt='logo'
                           priority={true}
                         />
                       </div>
                     </div>
-                    <h1 className="mb-4 mt-4 text-2xl font-bold text-center text-gray-700">
+                    <h1 className='mb-4 mt-4 text-2xl font-bold text-center text-gray-700'>
                       Öğretmen Giriş
                     </h1>
-                    <div className="mt-4">
-                      <label className="block text-sm">Email</label>
+                    <div className='mt-4'>
+                      <label className='block text-sm'>Email</label>
                       <input
                         id='email'
                         name='email'
@@ -147,12 +143,12 @@ const TeacherLoginComponent = ({ pageRole }) => {
                         type='email'
                         value={props.values.email}
                         onChange={props.handleChange}
-                        placeholder="Mail adresinizi giriniz."
-                        className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                        placeholder='Mail adresinizi giriniz.'
+                        className='w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600'
                       />
                     </div>
                     <div>
-                      <label className="block mt-4 text-sm">Şifre</label>
+                      <label className='block mt-4 text-sm'>Şifre</label>
 
                       <input
                         id='password'
@@ -160,27 +156,56 @@ const TeacherLoginComponent = ({ pageRole }) => {
                         type='password'
                         value={props.values.password}
                         onChange={props.handleChange}
-                        placeholder="******"
-                        className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                        placeholder='******'
+                        className='w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600'
                       />
                     </div>
                     <button
                       disabled={isAccessing}
-                      className={`${isAccessing ? "bg-gray-600 active:bg-gray-600 hover:bg-gray-600" : "bg-blue-600 active:bg-blue-600 hover:bg-blue-700"} block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150  border border-transparent rounded-lg  focus:outline-none focus:shadow-outline-blue`}
-                      type="submit"
+                      className={`${
+                        isAccessing
+                          ? 'bg-gray-600 active:bg-gray-600 hover:bg-gray-600'
+                          : 'bg-blue-600 active:bg-blue-600 hover:bg-blue-700'
+                      } block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150  border border-transparent rounded-lg  focus:outline-none focus:shadow-outline-blue`}
+                      type='submit'
                     >
                       Giriş Yap
                     </button>
 
-                    <div className="mt-4 text-center gap-2 flex justify-center items-center flex-col">
-                      <p className="text-md">
-                        Kayıtlı hesabınız yok mu?<Link href={`/auth/register/${pageRole.toLowerCase()}`} className="text-blue-600 hover:underline"> Öğretmen Kayıt.</Link>
+                    <div className='mt-4 text-center gap-2 flex justify-center items-center flex-col'>
+                      <p className='text-md text-black'>
+                        Kayıtlı hesabınız yok mu?
+                        <Link
+                          href={`/auth/register/${pageRole.toLowerCase()}`}
+                          className='text-blue-600 hover:underline'
+                        >
+                          {' '}
+                          Öğretmen Kayıt.
+                        </Link>
                       </p>
-                      <p className="text-md">
-                        Şifrenizi mi unuttunuz?<Link href={`/auth/forgotPassword/`} className="text-blue-600 hover:underline"> Şifremi Unuttum.</Link>
+                      <p className='text-md text-black'>
+                        Şifrenizi mi unuttunuz?
+                        <Link
+                          href={`/auth/forgotPassword/`}
+                          className='text-blue-600 hover:underline'
+                        >
+                          {' '}
+                          Şifremi Unuttum.
+                        </Link>
                       </p>
-                      <p className={`text-md bg-cst_red p-2 rounded ${popupData.popupIsActive ? "block" : "hidden"}`}>
-                        Mailiniz Doğrulanmamış mı?<Link href={`/auth/sendVerifyEmail`} className="text-blue-600 hover:underline"> Mail Doğrulama.</Link>
+                      <p
+                        className={`text-md bg-cst_red p-2 rounded ${
+                          popupData.popupIsActive ? 'block' : 'hidden'
+                        }`}
+                      >
+                        Mailiniz Doğrulanmamış mı?
+                        <Link
+                          href={`/auth/sendVerifyEmail`}
+                          className='text-blue-600 hover:underline'
+                        >
+                          {' '}
+                          Mail Doğrulama.
+                        </Link>
                       </p>
                     </div>
                   </div>
@@ -192,6 +217,6 @@ const TeacherLoginComponent = ({ pageRole }) => {
       </div>
     </>
   );
-}
+};
 
 export default TeacherLoginComponent;
