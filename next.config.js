90baaef9f25f721edf2a51dbcd390e247fc5b35b
yusuf <ysfyldz0077@gmail.com>
2024-03-21 14:24:32 +0300

    x

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => [
-    {
-      source: "/public/01. 4. Sınıf Matematik - 1. Ünite -  4-5-6 Basamaklı Sayılar - Test/story.html",
-      destination: "/pages/api/games/1.js",
-    },
-  ],
   experimental: {
     //appDir: true,
   },
   images: {
-    domains: ['source.unsplash.com','images.unsplash.com','free4kwallpapers.com'],
+    domains: ['source.unsplash.com', 'images.unsplash.com', 'free4kwallpapers.com'],
   },
-  
+
 }
 
 module.exports = nextConfig
