8950056ab62cd103361272055e0b66a7c2a84eb2
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-03-30 02:06:24 +0300

    redux removed.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,9 @@
-'use client'
-import '@/styles/global.css'
+'use client';
+import '@/styles/global.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { SessionProvider } from 'next-auth/react';
-
-import { Provider } from 'react-redux';
-import store from '@/redux/stores/index';
 import Navbar from '@/components/other/navbar';
-import { ThemeProvider } from "next-themes";
-
+import { ThemeProvider } from 'next-themes';
 
 /*
 import { signIn, signOut, useSession } from 'next-auth/react'
@@ -18,87 +14,106 @@ data?.user?.role -> kullanıcının rolüne erişebiliriz.
 3- student
 */
 const links = [
-    { url: '/', text: 'Ana Sayfa' },
-    {
-        url: '#',
-        text: 'Giriş Yap',
-        bannedRoles: ['admin', 'teacher', 'student'],
-        submenu: [
-            { url: '/auth/login/admin', text: 'Admin Giriş', bannedRoles: ['teacher', 'student'] },
-            { url: '/auth/login/teacher', text: 'Öğretmen Giriş', bannedRoles: ['teacher', 'student'] },
-            { url: '/auth/login/student', text: 'Öğrenci Giriş', bannedRoles: ['teacher', 'student'] },
-        ],
-    },
-    {
-        url: '#',
-        text: 'Kayıt Ol',
-        bannedRoles: ['admin', 'teacher', 'student'],
-        submenu: [
-            { url: '/auth/register/teacher', text: 'Öğretmen Kayıt', bannedRoles: ['teacher', 'student'] },
-            { url: '/auth/register/student', text: 'Öğrenci Kayıt', bannedRoles: ['teacher', 'student'] },
-        ],
-    },
+  { url: '/', text: 'Ana Sayfa' },
+  {
+    url: '#',
+    text: 'Giriş Yap',
+    bannedRoles: ['admin', 'teacher', 'student'],
+    submenu: [
+      {
+        url: '/auth/login/admin',
+        text: 'Admin Giriş',
+        bannedRoles: ['teacher', 'student'],
+      },
+      {
+        url: '/auth/login/teacher',
+        text: 'Öğretmen Giriş',
+        bannedRoles: ['teacher', 'student'],
+      },
+      {
+        url: '/auth/login/student',
+        text: 'Öğrenci Giriş',
+        bannedRoles: ['teacher', 'student'],
+      },
+    ],
+  },
+  {
+    url: '#',
+    text: 'Kayıt Ol',
+    bannedRoles: ['admin', 'teacher', 'student'],
+    submenu: [
+      {
+        url: '/auth/register/teacher',
+        text: 'Öğretmen Kayıt',
+        bannedRoles: ['teacher', 'student'],
+      },
+      {
+        url: '/auth/register/student',
+        text: 'Öğrenci Kayıt',
+        bannedRoles: ['teacher', 'student'],
+      },
+    ],
+  },
 
-    {
-        url: '#',
-        text: 'Dashboards',
-        accessRoles: ['admin', 'teacher', 'student'],
-        submenu: [
-            { url: '/dashboard/admin', text: 'Admin Dashboard', accessRoles: ['admin'], },
-            {
-                url: '/dashboard/teacher',
-                text: 'Öğretmen Dashboard',
-                accessRoles: ['teacher'],
-                bannedRoles: ['admin', 'student']
-            },
-            {
-                url: '/dashboard/student',
-                text: 'Öğrenci Dashboard',
-                accessRoles: ['student'],
-                bannedRoles: ['admin', 'teacher']
-            },
-        ],
-    },
+  {
+    url: '#',
+    text: 'Dashboards',
+    accessRoles: ['admin', 'teacher', 'student'],
+    submenu: [
+      {
+        url: '/dashboard/admin',
+        text: 'Admin Dashboard',
+        accessRoles: ['admin'],
+      },
+      {
+        url: '/dashboard/teacher',
+        text: 'Öğretmen Dashboard',
+        accessRoles: ['teacher'],
+        bannedRoles: ['admin', 'student'],
+      },
+      {
+        url: '/dashboard/student',
+        text: 'Öğrenci Dashboard',
+        accessRoles: ['student'],
+        bannedRoles: ['admin', 'teacher'],
+      },
+    ],
+  },
 
-    // {
-    //   url: '/products',
-    //   text: 'Products',
-    //   accessRoles: ['admin'],
-    //   bannedRoles: ['teacher', 'student'],
-    //   submenu: [
-    //     { url: '/auth/login/admin', text: 'Admin Giriş', accessRoles: ['admin'], bannedRoles: ['teacher', 'student'] },
-    //     { url: '/products/category2', text: 'Category 5' },
-    //     { url: '/products/category3', text: 'Category 6' },
-    //   ],
-    // },
-    // {
-    //   url: '/contact',
-    //   text: 'Contact',
-    //   button: true,
-    // },
-    // Diğer linkleri burada da tanımlayabilirsiniz
+  // {
+  //   url: '/products',
+  //   text: 'Products',
+  //   accessRoles: ['admin'],
+  //   bannedRoles: ['teacher', 'student'],
+  //   submenu: [
+  //     { url: '/auth/login/admin', text: 'Admin Giriş', accessRoles: ['admin'], bannedRoles: ['teacher', 'student'] },
+  //     { url: '/products/category2', text: 'Category 5' },
+  //     { url: '/products/category3', text: 'Category 6' },
+  //   ],
+  // },
+  // {
+  //   url: '/contact',
+  //   text: 'Contact',
+  //   button: true,
+  // },
+  // Diğer linkleri burada da tanımlayabilirsiniz
 ];
 
-
 const RootLayout = ({ children, session }) => {
-    return (
-        <html lang="tr">
+  return (
+    <html lang='tr'>
+      <head />
+      <body>
+        <ThemeProvider>
+          {/* SessionProvider ile sarmallarız ki tüm route lara erişebilelim diye / yukarıda "use client" tanımlamayı unutma! */}
+          <SessionProvider session={session}>
+            {/* <Navbar links={links}/> */}
+            {children}
+          </SessionProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
 
-            <head />
-            <body>
-                {/* Redux için Ana Layout yapımızı sarmallıyoruz ve store'u içine prop olarak gönderiyoruz. */}
-                <Provider store={store}>
-                    <ThemeProvider>
-                        {/* SessionProvider ile sarmallarız ki tüm route lara erişebilelim diye / yukarıda "use client" tanımlamayı unutma! */}
-                        <SessionProvider session={session}>
-                            {/* <Navbar links={links}/> */}
-                            {children}
-                        </SessionProvider>
-                    </ThemeProvider>
-                </Provider>
-            </body>
-        </html>
-    )
-}
-
-export default RootLayout
+export default RootLayout;
