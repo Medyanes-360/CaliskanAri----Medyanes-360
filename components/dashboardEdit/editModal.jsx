d3a06cf88a08cbc19da6e5c5993b33a8f8688ba4
Bayram Çınar <bayram200212@gmail.com>
2024-04-29 12:08:49 +0300

    apiTest

--- components/dashboardEdit/editModal.jsx
+++ components/dashboardEdit/editModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { SketchPicker } from "react-color";
 import Image from "next/image";
-import { postAPI } from "@/services/fetchAPI";
+import { postAPI, getAPI } from "@/services/fetchAPI";
 
 const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
   const [selectedColor, setSelectedColor] = useState("#000000");
@@ -303,95 +303,15 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
       label: "SEO Optimize",
     },
   ]); //FOOTER KURSLAR DEĞİŞKENİ
-  const [featured, setFeatured] = useState([
-    {
-      title: "The Complete Android Development Course",
-      name: "- Floyd Miles",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_1-380x277.jpg",
-      price: "",
-      star: 0,
-      topDesc: "Business",
-    },
-    {
-      title: "The Complete Android Java Developer Course",
-      name: "-  Millar Richard",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_2-380x277.jpg",
-      price: "",
-      discount: "",
-      star: 4,
-      topDesc: "Technology",
-      students: 11,
-      lessons: 8,
-    },
-    {
-      title: "The Complete WordPress Plugin Development Course",
-      name: "-  Jenny Wilson",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_3-380x277.jpg",
-      price: "",
-      star: 4,
-      topDesc: "Development",
-      students: 5,
-      lessons: 6,
-    },
-    {
-      title: "The Complete React Native Course",
-      name: "- Floyd Miles",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_5-380x277.jpg",
-      price: "Free",
-      star: 0,
-      topDesc: "Finance",
-      students: 7,
-      lessons: 5,
-    },
-    {
-      title: "JQuery and JavaScript Fundamental Course",
-      name: "- Jenny Wilson",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_4-380x277.jpg",
-      price: "",
-      star: 0,
-      topDesc: "Business",
-      students: 1,
-      lessons: 5,
-    },
-    {
-      title: "The Complete Cyber Security Course",
-      name: "- Kristin Watson",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_9-380x277.jpg",
-      price: "",
-      star: 0,
-      topDesc: "Technology",
-      students: 1,
-      lessons: 0,
-    },
-    {
-      title: "The Complete SEO & Digital Marketing Course",
-      name: "- Millar Richard",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_11-768x557.jpg",
-      price: "",
-      star: 5,
-      topDesc: "Technology",
-      students: 3,
-      lessons: 0,
-    },
-    {
-      title: "The Complete C and C++ Course for Beginners",
-      name: "- Eleanor Pena",
-      image:
-        "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_8jpg-768x557.jpg",
-      price: "",
-      star: 0,
-      topDesc: "Development",
-      students: 0,
-      lessons: 0,
-    },
-  ]); //FEATURED DEĞİŞKENİ
+  const featuresData = getAPI("/home/HomeFeatured");
+  const [featured, setFeatured] = useState([]); //FEATURED DEĞİŞKENİ
+  featuresData
+    .then(function (result) {
+      setFeatured(result);
+    })
+    .catch(function (error) {
+      console.error("Hata oluştu:", error);
+    });
   const [newCourse, setNewCourse] = useState({
     title: "",
     quantity: "",
@@ -521,11 +441,16 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
       prevCourses.filter((menu, index) => index !== indexToDelete)
     );
   }; //KURS SİLME
-  const deleteFeature = (indexToDelete) => {
-    setFeatured((prevFeature) =>
-      prevFeature.filter((menu, index) => index !== indexToDelete)
+  const deleteFeature = async (deleteObject) => {
+    console.log(deleteObject);
+    const response = await postAPI(
+      "/home/deleteFeature",
+      deleteObject,
+      "DELETE"
     );
+    console.log("API response:", response);
   }; //DERS SİLME
+
   const deleteFeatureCategory = (indexToDelete) => {
     setCategories((prevFeatureCategories) =>
       prevFeatureCategories.filter((menu, index) => index !== indexToDelete)
@@ -1213,7 +1138,7 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
                           </button>
                           <button
                             type="button"
-                            onClick={() => deleteNavbarMenu(menuIndex)}
+                            onClick={() => deleteNavbarMenu(menu)}
                             className="text-gray-600 bg-gray-100 py-3 px-5 rounded-xl font-semibold m-5 mr-0 ml-0"
                           >
                             <i class="fa-solid fa-trash"></i>
@@ -1264,7 +1189,7 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
                           </button>
                           <button
                             type="button"
-                            onClick={() => deleteFeature(index)}
+                            onClick={() => deleteFeature(feature)}
                             className="text-gray-600 bg-gray-100 py-3 px-5 rounded-xl font-semibold m-5 mr-0 ml-0"
                           >
                             <i class="fa-solid fa-trash"></i>
@@ -1313,7 +1238,7 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
 
                           <button
                             type="button"
-                            onClick={() => deleteFeatureCategory(index)}
+                            onClick={() => deleteFeatureCategory(category)}
                             className="text-gray-600 bg-gray-100 py-3 px-5 rounded-xl font-semibold m-5 mr-0 ml-0"
                           >
                             <i class="fa-solid fa-trash"></i>
@@ -1364,7 +1289,7 @@ const EditModal = ({ isOpen, onClose, modalContent, pageId }) => {
                           </button>
                           <button
                             type="button"
-                            onClick={() => deleteCourse(index)}
+                            onClick={() => deleteCourse(course)}
                             className="text-gray-600 bg-gray-100 py-3 px-5 rounded-xl font-semibold m-5 mr-0 ml-0"
                           >
                             <i class="fa-solid fa-trash"></i>
--- pages/api/home/[page]/index.js
+++ pages/api/home/[page]/index.js
@@ -1,33 +1,48 @@
-import { getAllData, createNewDataMany } from '@/services/serviceOperations';
+import {
+  getAllData,
+  createNewDataMany,
+  deleteDataByAny,
+} from "@/services/serviceOperations";
 
 async function homeHandler(req, res) {
-  const section = req.url.split('/')[3];
+  const section = req.url.split("/")[3];
   const requestMethod = req.method;
   const data = req.body;
   switch (requestMethod) {
-    case 'GET':
+    case "GET":
       try {
         const data = await getAllData(section);
         return res.status(200).json(data);
       } catch (error) {
         return res.status(500).json({ error: error.message });
       }
-    case 'POST':
+    case "DELETE":
       switch (section) {
-        case 'HomeInfo':
+        case "deleteFeature":
+          try {
+            console.log(data);
+            const response = await deleteDataByAny("HomeFeatured", data);
+            return res.status(200).json(response);
+          } catch (error) {
+            return res.status(500).json({ error: error.message });
+          }
+      }
+    case "POST":
+      switch (section) {
+        case "HomeInfo":
           return;
-        case 'addFeature':
+        case "addFeature":
           data.forEach((course) => {
             course.students = parseInt(course.students);
             course.lessons = parseInt(course.lessons);
           });
-          const response = await createNewDataMany('HomeFeatured', data);
+          const response = await createNewDataMany("HomeFeatured", data);
           console.log(response);
-          return res.status(200).json({ message: 'test', data: data });
+          return res.status(200).json({ message: "test", data: data });
       }
       break;
     default:
-      res.setHeader('Allow', ['GET', 'POST']);
+      res.setHeader("Allow", ["GET", "POST", "DELETE"]);
       return res.status(405).end(`Method ${requestMethod} Not Allowed`);
   }
 }
